f472f55eeb793e1a67d82a0bfd96d3b3c7184025
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2022-03-17 06:17:55 -0300

    Adicionando o metodo put

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
@@ -24,7 +24,7 @@ function checksExistsUserAccount(request, response, next) {
   return next();
 }
 
-app.post('/users', (request, response) => {
+app.post("/users", (request, response) => {
   const { name, username } = request.body;
 
   const AlredyExist = users.some((users) => users.username === username);
@@ -32,24 +32,24 @@ app.post('/users', (request, response) => {
   if (AlredyExist) {
     return response.status(400).json({ error: "Username alredy exist!" });
   }
-
-  users.push({
+  const user = {
     id: uuidv4(),
     name,
     username,
     todos: [],
-  })
+  };
+  users.push(user);
 
-  return response.status(201).send();
+  return response.status(201).json(user);
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
+app.get("/todos", checksExistsUserAccount, (request, response) => {
   const { user } = request;
 
-  return response.json(user)
+  return response.json(user);
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
+app.post("/todos", checksExistsUserAccount, (request, response) => {
   const { title, deadline } = request.body;
   const { user } = request;
 
@@ -59,30 +59,32 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
     done: false,
     deadline: new Date(deadline),
     created_at: new Date(),
-  }
+  };
   user.todos.push(createNewTodos);
-
-  return response.status(200).send();
-});
-
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  const { user } = request;
-  const { id } = request.query;
-  const { title, deadline } = request.body;
-
-  console.log(id)
-
-  user.title = title;
-  user.deadline = deadline;
 
   return response.status(201).send();
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
+app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
+  const { username } = request;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+  const index = users.findIndex((user) => user.username === username);
+  const todoIndex = users[index].todos.findIndex((todo) => todo.id === id);
+
+  console.log(id);
+
+  users[index].todos[todoIndex].title = title;
+  users[index].todos[todoIndex].deadline = deadline;
+
+  return response.status(200).send();
+});
+
+app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
